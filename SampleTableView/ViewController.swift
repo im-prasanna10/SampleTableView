5cb52dc3ad91e2d79882f944696fc3f262d14f21
prasanna-arumugam_tmna <prasanna.arumugam@toyota.com>
2023-08-16 11:57:55 +0530

    Initial Commit

---
+++ SampleTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleTableView
+//
+//  Created by Prasanna Venkatesh on 16/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
