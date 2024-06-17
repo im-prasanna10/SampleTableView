b8577d13231398b3f78467610add68ed8431760c
im-prasanna10 <prasannavenkatesh10.pv@gmail.com>
2024-06-17 20:08:52 +0530

    Code updated

--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arr = ["SRK", "RK" , "AK" , "VJ"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+     
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
+         return arr.count
+     }
 
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+         cell.textLabel?.text = arr[indexPath.row]
+         return cell
+     }
 }
+
 
